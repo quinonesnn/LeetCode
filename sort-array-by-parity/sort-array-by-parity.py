20d693a2baf77d66c30cf344b5dde5d04e941ac4
Nick Quinones <55732180+quinonesnn@users.noreply.github.com>
2022-06-09 10:05:11 -0400

    Time: 57 ms, Memory: 14.3 MB - LeetHub

--- sort-array-by-parity/sort-array-by-parity.py
+++ sort-array-by-parity/sort-array-by-parity.py
@@ -4,14 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        if len(nums) == 1:
-            return nums
-        for i in range(len(nums)):
-            if nums[i] % 2 != 0:
-                for j in range(i, len(nums)):
-                    if nums[j] % 2 == 0:
-                        odd = nums[i]
-                        nums[i] = nums[j]
-                        nums[j] = odd                        
-        return nums
-                        
+        even = []
+        odd = []
+        for num in nums:
+            if num % 2 == 0:
+                even.append(num)
+            else:
+                odd.append(num)
+        return even + odd
