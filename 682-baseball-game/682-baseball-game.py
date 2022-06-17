6ca81e980a55b579d708f102ec6e2ce09ffaf426
Nick Quinones <55732180+quinonesnn@users.noreply.github.com>
2022-06-17 15:34:43 -0400

    Time: 35 ms (59.00%), Space: 13.9 MB (56.00%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def calPoints(self, ops):
+        """
+        :type ops: List[str]
+        :rtype: int
+        """
+        report = []
+        for i in range(len(ops)):
+            if ops[i] == "+":
+                report.append(report[len(report) - 2] + report[len(report) -1])
+            if ops[i] == "D":
+                report.append(report[len(report) - 1] * 2)
+            if ops[i] == "C":
+                report.pop(len(report) - 1)
+            if ops[i].isnumeric():
+                report.append(int(ops[i]))
+            if ops[i][0] == "-":
+                print("negative")
+                report.append(int(ops[i]))
+        return sum(report)
