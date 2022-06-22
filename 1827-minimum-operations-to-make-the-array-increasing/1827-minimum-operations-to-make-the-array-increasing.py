7b2d61ec8293b236055a462ca45feda151c3dab3
Nick Quinones <55732180+quinonesnn@users.noreply.github.com>
2022-06-22 11:15:23 -0400

    Time: 149 ms (40.00%), Space: 14.2 MB (29.47%) - LeetHub

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(nums) - 1):
+            if nums[i] >= nums[i + 1]:
+                increment = nums[i] - nums[i + 1] + 1
+                nums[i + 1] += increment
+                count += increment
+        return count
+    
+    
+        """while nums[i] >= nums[i + 1]:
+        nums[i + 1] += 1
+        count += 1"""
+        
